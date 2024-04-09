6b7d8564cb44e5f8e52c302c3d76b1bb4d27737d
Vine <vinimassaroli@hotmail.com>
2024-04-09 20:06:21 -0300

    changed db

--- server.js
+++ server.js
@@ -40,14 +40,14 @@ var express = require("express");
 var client_1 = require("@prisma/client");
 var app = express();
 var prisma = new client_1.PrismaClient();
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Origin", "https://vineeeee.github.io");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     res.header("Access-Control-Allow-Origin", "https://vineeeee.github.io");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     next();
+// });
 app.use(express.json());
 app.get("/", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
     var tecnology, crew, destinations, error_1;
