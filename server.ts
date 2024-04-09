5accec0b6dbc513efe8ec0dc78523cc1caea57de
Vine <vinimassaroli@hotmail.com>
2024-04-09 20:08:37 -0300

    changed db

--- server.ts
+++ server.ts
@@ -4,14 +4,14 @@ import { PrismaClient } from "@prisma/client";
 const app = express();
 const prisma = new PrismaClient();
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//     res.header("Access-Control-Allow-Origin", "https://vineeeee.github.io");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Origin", "https://banco-de-dados-planetas-three.vercel.app/");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 app.use(express.json());
 
